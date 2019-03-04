31ae0d978fb0ef349c3af090deb6da25b232ad11
Damien Elmes <gpg@ankiweb.net>
2019-03-05 08:57:53 +1000

    add option to strip html in export

--- tests/test_exporting.py
+++ tests/test_exporting.py
@@ -16,7 +16,7 @@ def setup1():
     global deck
     deck = getEmptyCol(scheduler='anki.schedv3.Scheduler')
     f = deck.newNote()
-    f['Front'] = "foo"; f['Back'] = "bar"; f.tags = ["tag", "tag2"]
+    f['Front'] = "foo"; f['Back'] = "bar<br>"; f.tags = ["tag", "tag2"]
     deck.addNote(f)
     # with a different deck
     f = deck.newNote()
@@ -134,8 +134,11 @@ def test_export_textnote():
     os.close(fd)
     os.unlink(f)
     e.exportInto(f)
-    e.includeTags = True
+    assert open(f).readline() == "foo\tbar<br>\ttag tag2\n"
+    e.includeTags = False
+    e.includeHTML = False
     e.exportInto(f)
+    assert open(f).readline() == "foo\tbar\n"
 
 def test_exporters():
     assert "*.apkg" in str(exporters())

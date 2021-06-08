28440b1a4e3b639b59cd76fdec863510719b989b
Sławomir Domagała <slawomir.karol.domagala@gmail.com>
2021-06-08 23:56:12 +0200

    Improve stats2

--- aqt/stats2.py
+++ aqt/stats2.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
+
+import numpy
+import pandas
+import pyqtgraph
 from PyQt5.QtChart import QLineSeries, QChart, QChartView, QBarSeries, QBarSet, QStackedBarSeries, QBarCategoryAxis
 from PyQt5.QtCore import QPointF, Qt
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QMainWindow, QGridLayout, QWidget
+from matplotlib import pyplot
+from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
 
+from anki.database.revision_answers import RevisionAnswer
 from anki.lang import _
 from anki.stats import CollectionStats
 from anki.utils import ids2str
@@ -23,6 +31,7 @@ class Stats2Window(QMainWindow):
 
         self._create_reviews_due_chart()
         self._create_revision_count_graph()
+        self._create_expected_answered_ease_heatmap()
 
     def _create_reviews_due_chart(self):
         ## TODO: later replace by date selector
@@ -30,20 +39,23 @@ class Stats2Window(QMainWindow):
         stats = CollectionStats(self.col)
 
         ## TODO: refactor stats._due
+        categories_labels = list(range(0, 31, 1))
         data = stats.due(start, end, chunk)
+        data = self._transform_first_column_to_key(data)
+        data = self._add_default_value_for_missing_keys(data, [0, 0, 0, 0], categories_labels)
 
         series = QStackedBarSeries(self)
         setYoung = QBarSet("Young", self)
-        [setYoung.append(row[1]) for row in data]
+        [setYoung.append(row[0]) for row in data]
         setAdolescent = QBarSet("Adolescent", self)
-        [setAdolescent.append(row[2]) for row in data]
+        [setAdolescent.append(row[1]) for row in data]
         setMature = QBarSet("Mature", self)
-        [setMature.append(row[3]) for row in data]
+        [setMature.append(row[2]) for row in data]
         setOld = QBarSet("Old", self)
-        [setOld.append(row[4]) for row in data]
+        [setOld.append(row[3]) for row in data]
 
         categories = QBarCategoryAxis()
-        categories.setCategories([str(row[0]) for row in data])
+        categories.setCategories([str(i) for i in categories_labels])
 
         series.append(setOld)
         series.append(setMature)
@@ -69,23 +81,25 @@ class Stats2Window(QMainWindow):
     def _create_revision_count_graph(self):
         stats = CollectionStats(self.col)
 
+        categories_labels = list(range(-30, 1, 1))
         data = stats.revision_count_stats()
-        print(data)
+        data = self._transform_first_column_to_key(data)
+        data = self._add_default_value_for_missing_keys(data, [0, 0, 0, 0, 0], categories_labels)
 
         series = QStackedBarSeries(self)
         setLearning = QBarSet("Learning", self)
-        [setLearning.append(row[1]) for row in data]
+        [setLearning.append(row[0]) for row in data]
         setYoung = QBarSet("Young", self)
-        [setYoung.append(row[2]) for row in data]
+        [setYoung.append(row[1]) for row in data]
         setMature = QBarSet("Mature", self)
-        [setMature.append(row[3]) for row in data]
+        [setMature.append(row[2]) for row in data]
         setLapse = QBarSet("Lapse", self)
-        [setLapse.append(row[4]) for row in data]
+        [setLapse.append(row[3]) for row in data]
         setEarly = QBarSet("Early", self)
-        [setEarly.append(row[5]) for row in data]
+        [setEarly.append(row[4]) for row in data]
 
         categories = QBarCategoryAxis()
-        categories.setCategories([str(-i) for i in range(-31, 0, 1)])
+        categories.setCategories([str(-i) for i in categories_labels])
 
         series.append(setLearning)
         series.append(setYoung)
@@ -109,3 +123,42 @@ class Stats2Window(QMainWindow):
 
         self.layout.addWidget(chartview)
 
+    def _create_expected_answered_ease_heatmap(self):
+        chosen_ease = ['1', '2', '3', '4']
+        expected_ease = ['1', '2', '3']
+
+        answers = [[t.expected_ease, t.chosen_ease] for t in RevisionAnswer.select(RevisionAnswer.expected_ease, RevisionAnswer.chosen_ease)]
+        df = pandas.DataFrame(answers, columns=['expected_ease', 'chosen_ease']).groupby(['expected_ease', 'chosen_ease']).size().unstack(fill_value=0)
+        mat = df.to_numpy()
+        print(mat)
+
+        mw = MatplotlibWidget()
+        fig = mw.getFigure()
+        ax = fig.add_subplot()
+        im = ax.imshow(df)
+
+        ax.set_xticks(numpy.arange(len(chosen_ease)))
+        ax.set_yticks(numpy.arange(len(expected_ease)))
+
+        ax.set_xticklabels(chosen_ease)
+        ax.set_yticklabels(expected_ease)
+
+        ax.set_xlabel("Chosen ease")
+        ax.set_ylabel("Expected ease")
+
+        pyplot.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+        for i in range(len(chosen_ease)):
+            for j in range(len(expected_ease)):
+                text = ax.text(i, j, mat[j, i], ha="center", va="center", color="w")
+
+        fig.tight_layout()
+        mw.draw()
+        self.layout.addWidget(mw)
+
+    def _transform_first_column_to_key(self, data):
+        return {row[0]: row[1::] for row in data}
+
+    def _add_default_value_for_missing_keys(self, data, default_value, keys):
+        data = defaultdict(lambda: default_value, data)
+        return [data[key] for key in keys]
